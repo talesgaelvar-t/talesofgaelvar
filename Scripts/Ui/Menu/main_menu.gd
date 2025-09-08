9db0534eac597c8e3dce0b350fcdec49ec99a8d8
Damian <damianmorganti16@hotmail.com>
2025-09-07 20:37:21 -0400

    feat(ui): add Options scene with background and button functionality

--- Scripts/Ui/Menu/main_menu.gd
+++ Scripts/Ui/Menu/main_menu.gd
@@ -10,7 +10,7 @@ func _on_campaign_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/UI/Options/Options.tscn")
 
 
 func _on_quit_pressed() -> void:
